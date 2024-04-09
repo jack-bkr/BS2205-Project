df38a5e6af25ee288b3d4b9cc314b67b603633b6
jack-bkr <jackbarker04@icloud.com>
2024-04-09 20:50:43 +0100

    add viewItem page, showing price and description. also link to relevent page when clicking item figure

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -21,4 +21,19 @@ itemRouter.get('/add', function (req, res) {  // get request to root path
     res.sendFile(path.join(__dirname,'../../public/html/addItem.html'));
 });
 
+itemRouter.get('/:id', function (req, res) {
+    try {
+        const promise = fetch(dbURL.site + "/api/item/" + req.params.id).then((response) => {
+            if (response.status === 404) {
+                res.status(404).send({ message: "Item not found" });
+            }
+            return response.json().then((data) => {
+                res.render('../views/viewItem.ejs', data);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+})
+
 module.exports = itemRouter;
